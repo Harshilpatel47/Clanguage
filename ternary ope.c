3b0d5c4ba62ce543a54c1c515cbdfaf1414e08c6
Harshilpatel47 <162704949+Harshilpatel47@users.noreply.github.com>
2024-03-11 17:53:22 +0530

    momentum file upload

---
+++ ternary ope.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int a;
+	
+	printf("\n enter any values:");
+	scanf("%d",&a);
+	(a%2 ==0)? printf("\n This value is even no "):printf("\n This value is odd no");
+	
+}
